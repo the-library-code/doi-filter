ca1ecf4103ffe1a07b7c53a19bce9560dbff8361
Pascal-Nicolas Becker <dspace@pascal-becker.de>
2022-02-13 22:15:25 +0100

    Fix InCollectionCondition: check for Workflow and workspace items
    When an item gets installed, the filter might be called at a moment
    where no owning collection is set yet. If we do not find any collections
    we should look out for corresponding workflow and workspace items.

--- dspace-api/src/main/java/org/dspace/content/logic/condition/InCollectionCondition.java
+++ dspace-api/src/main/java/org/dspace/content/logic/condition/InCollectionCondition.java
@@ -7,14 +7,21 @@
  */
 package org.dspace.content.logic.condition;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.dspace.content.Collection;
 import org.dspace.content.Item;
+import org.dspace.content.WorkspaceItem;
+import org.dspace.content.factory.ContentServiceFactory;
 import org.dspace.content.logic.LogicalStatementException;
 import org.dspace.content.service.CollectionService;
 import org.dspace.core.Context;
+import org.dspace.services.factory.DSpaceServicesFactory;
+import org.dspace.workflow.WorkflowItem;
+import org.dspace.workflow.WorkflowService;
+import org.dspace.workflow.factory.WorkflowServiceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -34,13 +41,41 @@ public class InCollectionCondition extends AbstractCondition {
     public Boolean getResult(Context context, Item item) throws LogicalStatementException {
 
         List<String> collectionHandles = (List<String>)getParameters().get("collections");
-        List<Collection> itemCollections = item.getCollections();
+        List<Collection> itemCollections = new ArrayList<>();
+        itemCollections.addAll(item.getCollections());
+        
+        if (itemCollections.size() < 1) {
+            log.debug("No collection found for item " + item.getHandle()
+                    + ". Looking for corresponding workspace or workflow item.");
+            try {
+                // check for Workflow or Workspace item
+                WorkspaceItem wsi = ContentServiceFactory.getInstance().getWorkspaceItemService()
+                        .findByItem(context, item);
+                if (wsi != null) {
+                    Collection collection = wsi.getCollection();
+                    itemCollections.add(collection);
+                    log.debug("found workspace item and owning collection: " + collection.getHandle());
+                }
+                WorkflowItem wfi = WorkflowServiceFactory.getInstance().getWorkflowItemService()
+                        .findByItem(context, item);
+                if (wfi != null) {
+                    Collection collection = wfi.getCollection();
+                    itemCollections.add(collection);
+                    log.debug("found workflow item and owning collection: " + collection.getHandle());
+                }
+            } catch (Exception ex) {
+                log.debug("Caught exception while looking for workspace/workflow item:", ex);
+            }
+        }
 
         for(Collection collection : itemCollections) {
             if(collectionHandles.contains(collection.getHandle())) {
                 log.debug("item " + item.getHandle() + " is in collection "
                     + collection.getHandle() + ", returning true");
                 return true;
+            } else {
+                log.debug("item " + item.getHandle() + " is in collection "
+                    + collection.getHandle() + ", continue loop");
             }
         }
 
